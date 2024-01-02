4f8a3c1f80f78d0d4499ccd8cd5f8961ffd1158c
Supriyaratnaparkhe <91657954+Supriyaratnaparkhe@users.noreply.github.com>
2024-01-02 14:04:23 +0530

    Update ViewJob.jsx

--- src/Components/ViewJob/ViewJob.jsx
+++ src/Components/ViewJob/ViewJob.jsx
@@ -23,7 +23,7 @@ const ViewJob = () => {
   });
   const [isLoggedIn, setIsLoggedIn] = useState(isUserLoggedIn());
   const { jobId } = useParams();
-  axios.defaults.withCredentials = true;
+  // axios.defaults.withCredentials = true;
   useEffect(() => {
     try {
       setIsLoggedIn(isUserLoggedIn());
