fb001b5195c61caee0647554f5e0c28c9d54664c
Supriyaratnaparkhe <91657954+Supriyaratnaparkhe@users.noreply.github.com>
2024-02-12 15:19:30 +0530

    Update Home.jsx

--- src/Components/HomePage/Home.jsx
+++ src/Components/HomePage/Home.jsx
@@ -2,22 +2,33 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../Navbar/Navbar";
 import JobList from "../JobList/JobList";
 import { isUserLoggedIn } from "../../utils/utils";
+import Spinner from "../Spinner/Spinner";
 
 const Home = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(isUserLoggedIn());
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     try {
       setIsLoggedIn(isUserLoggedIn());
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   }, []);
   return (
-    <div>
-      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-      <JobList isLoggedIn={isLoggedIn} />
-    </div>
+    <>
+      {!loading ? (
+        <div>
+          <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+          <JobList isLoggedIn={isLoggedIn} />
+        </div>
+      ) : (
+        loading && <Spinner />
+      )}
+    </>
   );
 };
 
 export default Home;
+
